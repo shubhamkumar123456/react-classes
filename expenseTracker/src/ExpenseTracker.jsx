99c02044817e86a043ed80b473305231037fe338
shubham kumar <clboy768@gmail.com>
2025-08-19 19:15:30 +0530

    updted

--- expenseTracker/src/ExpenseTracker.jsx
+++ expenseTracker/src/ExpenseTracker.jsx
@@ -1,38 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useState } from 'react'
 
 const ExpenseTracker = () => {
 
-    // let arr = [
-    //     {
-    //         id: 1,
-    //         credited: 35000,
-    //         date: "01-08-2025",
-    //         debited: 0,
-    //         place: 'salary'
-    //     },
-    //     {
-    //         id: 2,
-    //         credited: 1500,
-    //         date: "05-08-2025",
-    //         debited: 0,
-    //         place: 'bua ke ghar'
-    //     },
-    //     {
-    //         id: 3,
-    //         credited: 0,
-    //         date: "05-08-2025",
-    //         debited: 1000,
-    //         place: 'dinner'
-    //     },
-    //     {
-    //         id: 4,
-    //         credited: 0,
-    //         date: "06-08-2025",
-    //         debited: 300,
-    //         place: 'zoo'
-    //     },
-    // ]
+    let creditRef = useRef()
+    let debitRef = useRef()
+    let placeRef = useRef()
+    let dateRef = useRef()
+  
+    
 
     const [arr, setArr] = useState([
         {
@@ -94,15 +70,42 @@ const ExpenseTracker = () => {
     }
 
 
-    function abc(a){
+    function handleSubmit(e){
+        e.preventDefault()
+        // console.log("hello")
+       
+        let obj = {
+            credited:creditRef.current.value,
+            debited:debitRef.current.value,
+            place:placeRef.current.value,
+            date:dateRef.current.value,
+        }
+        console.log(obj)
+        if(!obj.place && !obj.date){
+            alert("please fill required fields")
+            return
+        }
+        let copyArr = [...arr];
+        copyArr.push(obj)
 
+
+    
+        setArr(copyArr)
+        // arr.push(obj);
+        // console.log(arr)
     }
-
-    abc(10)
 
     return (
         <div className='expensePage'>
             <h1 className='title'>This is Expense Tracker Application</h1>
+
+            <form action="">
+                <input ref={creditRef} id='credit' type="number" placeholder='credit amount' />
+                <input ref={debitRef} id='debit' type="number" placeholder='debit amount' />
+                <input ref={placeRef} id='place' type="text" placeholder='Place' />
+                <input ref={dateRef} id='date' type="date" placeholder='date' />
+                <button onClick={handleSubmit}>Add Expense</button>
+            </form>
 
             <table border={1} cellPadding={'10px'}>
                 <thead>
@@ -121,8 +124,8 @@ const ExpenseTracker = () => {
                         arr.map((obj, i)=>{
                             return <tr key={obj.id} >
                         <td>{i+1}</td>
-                        <td>{obj.credited}</td>
-                        <td>{obj.debited}</td>
+                        <td>{obj.credited ==='' ? 0 : obj.credited}</td>
+                        <td>{obj.debited ===''?0 :obj.debited}</td>
                         <td>{obj.place}</td>
                         <td>{obj.date}</td>
                         <td><button onClick={()=>handleDelete(obj,i)}>delete</button></td>
