02221fff6b78c18c6fcbe37b476caf5b6d1a48de
shubham kumar <clboy768@gmail.com>
2025-12-29 19:29:15 +0530

    added hom  and login

--- taskFrontend/src/pages/Home.jsx
+++ taskFrontend/src/pages/Home.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Home = () => {
+  let userData = JSON.parse(localStorage.getItem('tasksAuth'))
+  console.log(userData)
+  console.log(userData._id)
+
+
+  let taskRef = useRef()
+  let dateRef = useRef()
+
+
+ async function handleSubmit(e){
+      e.preventDefault();
+      let obj ={
+        taskName:taskRef.current.value,
+        status:"incompleted",
+        date:dateRef.current.value
+      }
+      let res = await fetch(`http://localhost:8080/api/tasks/create/${userData._id}`,{
+        method:"POST",
+        headers:{
+          'content-type':"application/json"
+        },
+        body:JSON.stringify(obj)
+      })
+
+      let data = await res.json();
+      console.log(data)
+      toast.success(data.msg, {position:"top-center"})
+      getData()
+  }
+
+  const [arr, setarr] = useState([]);
+
+  async function getData(){
+      let res = await fetch(`http://localhost:8080/api/tasks/task/${userData._id}`);
+      let data = await res.json();
+      console.log(data)
+      console.log(data.data)
+      setarr(data.data)
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div>
       <h1>This is Home Page</h1>
+      <form action="">
+          <input ref={taskRef} className='border px-4 py-2 rounded  mx-2' type="text" placeholder='enter task name' />
+          <input ref={dateRef} className='border px-4 py-2 rounded  mx-2' type="date" />
+          <button onClick={handleSubmit} className='border px-4 py-2 rounded  mx-2'>Submit</button>
+      </form>
+
+      <table>
+          <thead>
+              <tr>
+                <th>Sno</th>
+                <th>TaskName</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+          </thead>
+
+          <tbody>
+            {
+              arr.map((ele, i)=>{
+                return <tr key={ele._id}>
+                    <td className='px-4'>{i+1}</td>
+                    <td>{ele.taskName}</td>
+                    <td>{ele.date}</td>
+                    <td>
+                      <button className='bg-green-500 px-4 py-2 rounded'>Delete Task</button>
+                    </td>
+                </tr>
+              })
+            }
+          </tbody>
+      </table>
     </div>
   )
 }
--- taskFrontend/src/pages/Login.jsx
+++ taskFrontend/src/pages/Login.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+    let navigate = useNavigate()
+    let emailRef = useRef()
+    let passwordRef = useRef()
 
-    // res  --> {msg:"login successfull" ,user:{id, name, email, password}};
+   async function handleSubmit(e){
+        e.preventDefault()
+        
+        let obj = {
+            email:emailRef.current.value,
+            password:passwordRef.current.value
+        }
 
-    localStorage. save(user)
-    Navigate('/')
+        console.log(obj)
 
+        try {
+            let res =await fetch('http://localhost:8080/api/users/login',{
+            method:"POST",
+            headers:{
+                'content-type':"application/json"
+            },
+            body:JSON.stringify(obj)
+        })
+
+        let data = await res.json();
+        console.log(data) // {msg, user}
+        if(res.status==200 ){
+            toast.success(data.msg)
+            localStorage.setItem('tasksAuth',JSON.stringify(data.user))
+            navigate('/')
+        }
+        else{
+            toast.error(data.msg)
+        }
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <div>
-      <h1>This is loginpage</h1>
+      <form action="" className='flex flex-col gap-2 w-[60%] m-auto'>
+        
+         <label htmlFor="">Email</label>
+         <input ref={emailRef} className='border px-4 py-2' type="email" placeholder='enter your email' />
+         <label htmlFor="">Password</label>
+         <input ref={passwordRef} className='border px-4 py-2' type="password" placeholder='enter your password' />
+         <button className='bg-green-300 px-4 py-2 rounded' onClick={handleSubmit}>Submit</button>
+      </form>
     </div>
   )
 }
